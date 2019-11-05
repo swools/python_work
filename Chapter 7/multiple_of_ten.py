7376edd4822ff5bf41a820cbf802285e027488ac
Steve Wool <swool@backstopsolutions.com>
2019-11-05 18:53:19 -0500

    started chapter 7 exercises

---
+++ Chapter 7/multiple_of_ten.py
@@ -0,0 +1,7 @@
+num = input("Enter a number ")
+num = int(num)
+
+if num % 10 == 0:
+    print("This number is divisible by 10")
+else:
+    print("This number is not divisible by 10")
---
+++ Chapter 7/restaurant_seating.py
@@ -0,0 +1,7 @@
+num = input("How many people are in your dining group? ")
+num = int(num)
+
+if num > 8:
+    print("Sorry you'll have to wait for a table")
+else:
+    print("Your table is ready")
